9dae4117f49c2667dd19090bfb7e4929c33199dc
thulasineom <thulasi.rajan@neom.com>
2022-02-18 20:44:08 +0530

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/basha/flutter_sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/basha/Downloads/fiberchat"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.33"
+export "FLUTTER_BUILD_NUMBER=34"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
